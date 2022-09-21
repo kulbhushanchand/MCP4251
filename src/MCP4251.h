e2b60f4d962a27d17cd406bf72b57e85899a54ed
David <david@plentify.io>
2022-09-21 19:02:37 +0200

    added defines for other two pots, changed all bools to uint8_t's

--- src/MCP4251.h
+++ src/MCP4251.h
@@ -7,15 +7,20 @@
 #include "Wprogram.h"
 #endif
 
-#define ADDRESS_WIPER_0 B00000000
-#define ADDRESS_WIPER_1 B00010000
-#define ADDRESS_TCON B01000000
-#define ADDRESS_STATUS B01010000
+#define ADDRESS_WIPER_0   B00000000
+#define ADDRESS_WIPER_1   B00010000
+#define ADDRESS_TCON0     B01000000
 
-#define COMMAND_WRITE B00000000
+#define ADDRESS_WIPER_2   B01100000
+#define ADDRESS_WIPER_3   B01110000
+#define ADDRESS_TCON1     B10100000
+
+#define ADDRESS_STATUS    B01010000
+
+#define COMMAND_WRITE     B00000000
 #define COMMAND_INCREMENT B00000100
 #define COMMAND_DECREMENT B00001000
-#define COMMAND_READ B00001100
+#define COMMAND_READ      B00001100
 
 #define DUMMY_DATA B11111111
 
@@ -23,54 +28,66 @@
 
 #define BITMASK_POT0_STARTUP B00001000
 #define BITMASK_POT1_STARTUP B10000000
+#define BITMASK_POT2_STARTUP B00001000
+#define BITMASK_POT3_STARTUP B10000000
+
 #define BITMASK_POT0_B_TERMINAL_CONNECT B00000001
 #define BITMASK_POT1_B_TERMINAL_CONNECT B00010000
+#define BITMASK_POT2_B_TERMINAL_CONNECT B00000001
+#define BITMASK_POT3_B_TERMINAL_CONNECT B00010000
+
 #define BITMASK_POT0_WIPER_TERMINAL_CONNECT B00000010
 #define BITMASK_POT1_WIPER_TERMINAL_CONNECT B00100000
+#define BITMASK_POT2_WIPER_TERMINAL_CONNECT B00000010
+#define BITMASK_POT3_WIPER_TERMINAL_CONNECT B00100000
+
 #define BITMASK_POT0_A_TERMINAL_CONNECT B00000100
 #define BITMASK_POT1_A_TERMINAL_CONNECT B01000000
-
+#define BITMASK_POT2_A_TERMINAL_CONNECT B00000100
+#define BITMASK_POT3_A_TERMINAL_CONNECT B01000000
 
 
 class MCP4251
 {
 public:
   // Constructor
-  MCP4251(uint8_t slaveSelectPin, float pot0ResistanceRmax, float pot0ResistanceRmin, float pot1ResistanceRmax, float pot1ResistanceRmin);
+  MCP4251(uint8_t slaveSelectPin, float pot0ResistanceRmax, float pot0ResistanceRmin, float pot1ResistanceRmax, float pot1ResistanceRmin, float pot2ResistanceRmax, float pot2ResistanceRmin, float pot3ResistanceRmax, float pot3ResistanceRmin);
   void begin();
-  void DigitalPotWiperIncrement(bool potNum);
-  void DigitalPotWiperDecrement(bool potNum);
-  void DigitalPotSetWiperPosition(bool potNum, uint16_t value); // Need confirmation if it is working
-  void DigitalPotSetWiperMin(bool potNum);
-  void DigitalPotSetWiperMax(bool potNum);
-  void DigitalPotSetWiperMid(bool potNum);
+  void DigitalPotWiperIncrement(uint8_t potNum);
+  void DigitalPotWiperDecrement(uint8_t potNum);
+  void DigitalPotSetWiperPosition(uint8_t potNum, uint16_t value); // Need confirmation if it is working
+  void DigitalPotSetWiperMin(uint8_t potNum);
+  void DigitalPotSetWiperMax(uint8_t potNum);
+  void DigitalPotSetWiperMid(uint8_t potNum);
 
-  uint16_t DigitalPotReadWiperPosition(bool potNum);
+  uint16_t DigitalPotReadWiperPosition(uint8_t potNum);
   uint16_t DigitalPotReadTconRegister();
   uint16_t DigitalPotReadStatusRegister();
 
-  void DigitalPotWriteTconRegister(uint16_t value);
+  void DigitalPotWriteTcon0Register(uint16_t value);
+  void DigitalPotWriteTcon1Register(uint16_t value);
 
-  void DigitalPotStartup(bool potNum);
-  void DigitalPotShutdown(bool potNum);
+  void DigitalPotStartup(uint8_t potNum);
+  void DigitalPotShutdown(uint8_t potNum);
 
-  void DigitalPotTerminalBConnect(bool potNum);
-  void DigitalPotTerminalBDisconnect(bool potNum);
+  void DigitalPotTerminalBConnect(uint8_t potNum);
+  void DigitalPotTerminalBDisconnect(uint8_t potNum);
 
-  void DigitalPotTerminalAConnect(bool potNum);
-  void DigitalPotTerminalADisconnect(bool potNum);
+  void DigitalPotTerminalAConnect(uint8_t potNum);
+  void DigitalPotTerminalADisconnect(uint8_t potNum);
 
-  void DigitalPotWiperConnect(bool potNum);
-  void DigitalPotWiperDisconnect(bool potNum);
+  void DigitalPotWiperConnect(uint8_t potNum);
+  void DigitalPotWiperDisconnect(uint8_t potNum);
 
   void DigitalPotInitTcon();
 
-  uint16_t DigitalPotResistanceToPosition(bool potNum, float resistance);
-  float DigitalPotPositionToResistance(bool potNum, uint16_t position);
+  uint16_t DigitalPotResistanceToPosition(uint8_t potNum, float resistance);
+  float DigitalPotPositionToResistance(uint8_t potNum, uint16_t position);
 
 private:
   uint8_t _slaveSelectPin;
-  float _pot0ResistanceRmax, _pot0ResistanceRmin, _pot0ResistanceRAB, _pot0ResistanceRW, _pot1ResistanceRmax, _pot1ResistanceRmin, _pot1ResistanceRAB, _pot1ResistanceRW;
+  float _pot0ResistanceRmax, _pot0ResistanceRmin, _pot0ResistanceRAB, _pot0ResistanceRW, _pot1ResistanceRmax, _pot1ResistanceRmin, _pot1ResistanceRAB, _pot1ResistanceRW, \
+        _pot2ResistanceRmax, _pot2ResistanceRmin, _pot2ResistanceRAB, _pot2ResistanceRW, _pot3ResistanceRmax, _pot3ResistanceRmin, _pot3ResistanceRAB, _pot3ResistanceRW;
 
 };
 
